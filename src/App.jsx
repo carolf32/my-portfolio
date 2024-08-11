380a0c52e28bc5f55abe210f9d820680fce15e55
Carolina do Amaral <carolinafrederico32@hotmail.com>
2024-08-11 17:51:17 +0100

    melhorias no about me

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,16 @@ function App() {
     <RefsProvider>
       <Header />
       <Banner />
+      <div className="hidden lg:flex justify-center ">
+        <iframe
+          className="dark:hidden"
+          src="https://lottie.host/embed/fe17fdce-104a-48cf-ae96-2b9845470523/WFxO5GoxTS.json"
+        ></iframe>
+        <iframe
+          className="hidden dark:block"
+          src="https://lottie.host/embed/228377da-72f1-4379-88ed-7b434b093568/Thf5Fc4Bwq.json"
+        ></iframe>
+      </div>
       <AboutMe />
       <Stack />
       <Projects />
--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -11,16 +11,6 @@ export const AboutMe = () => {
       ref={aboutMeRef}
       className="flex flex-col text-center pb-0 lg:pb-6 pt-6 lg:pt-0 px-10 md:px-15 lg:px-20 "
     >
-      <div className="mx-auto hidden lg:block ">
-        <iframe
-          className="dark:hidden"
-          src="https://lottie.host/embed/fe17fdce-104a-48cf-ae96-2b9845470523/WFxO5GoxTS.json"
-        ></iframe>
-        <iframe
-          className="hidden dark:block"
-          src="https://lottie.host/embed/228377da-72f1-4379-88ed-7b434b093568/Thf5Fc4Bwq.json"
-        ></iframe>
-      </div>
       <Fade duration="3000" triggerOnce={false} fraction="0.2">
         <h2 className={`text-3xl font-semibold my-10 `}>About Me</h2>
         <div className="flex flex-col md:flex-row justify-center items-center">
@@ -41,25 +31,40 @@ export const AboutMe = () => {
               <h2 className="text-xl font-semibold mb-3 ">
                 🌍 Language skills
               </h2>
-              <div className={`${style.blackBorder} rounded-full text-sm my-2`}>
+
+              <div className="flex justify-between items-center ">
+                <h3 className="font-semibold px-3 w-1/4">Portuguese</h3>
+
                 <div
-                  className={`rounded-full bg-main-color m-0.5 w-auto text-white `}
+                  className={`${style.blackBorder} rounded-full text-sm my-2  w-3/4`}
                 >
-                  Portuguese (Native)
+                  <div
+                    className={`rounded-full bg-main-color m-0.5 w-auto text-white `}
+                  >
+                    Native
+                  </div>
                 </div>
               </div>
-              <div
-                className={`${style.blackBorder} rounded-full text-sm my-2 text-white `}
-              >
-                <div className={`rounded-full bg-main-color m-0.5 w-4/5`}>
-                  English (C1 Level)
+
+              <div className="flex justify-between items-center ">
+                <h3 className="font-semibold px-3 w-1/4">English</h3>
+                <div
+                  className={`${style.blackBorder} rounded-full text-sm my-2 text-white  w-3/4`}
+                >
+                  <div className={`rounded-full bg-main-color m-0.5 w-4/5`}>
+                    C1 Level
+                  </div>
                 </div>
               </div>
-              <div
-                className={`${style.blackBorder} rounded-full text-sm my-2 text-white font-`}
-              >
-                <div className={`rounded-full bg-main-color m-0.5 w-1/3`}>
-                  Spanish (B1 Level)
+
+              <div className="flex justify-between items-center ">
+                <h3 className="font-semibold px-3 w-1/4">Spanish</h3>
+                <div
+                  className={`${style.blackBorder} rounded-full text-sm my-2 text-white w-3/4`}
+                >
+                  <div className={`rounded-full bg-main-color m-0.5 w-1/3`}>
+                    B1 Level
+                  </div>
                 </div>
               </div>
             </div>
